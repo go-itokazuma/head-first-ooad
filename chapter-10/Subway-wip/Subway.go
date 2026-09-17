beb08ed6e5d2915c426b29ef8678732da7576fed
kazuma.ito <kazuma.ito@goinc.jp>
2026-09-17 11:24:35 +0900

    イテレーション1　地下鉄の路線網をロードする

--- chapter-10/Subway-wip/Subway.go
+++ chapter-10/Subway-wip/Subway.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Subway struct {
 	stations    []*Station
@@ -31,6 +34,33 @@ func (s *Subway) AddStation(stationName string) {
 	}
 }
 
+func (s *Subway) AddConnection(station1Name string, station2Name string, lineName string) {
+	if s.HasStation(station1Name) && s.HasStation(station2Name) {
+		station1 := NewStation(station1Name)
+		station2 := NewStation(station2Name)
+		connection := NewConnection(station1, station2, lineName)
+		s.connections = append(s.connections, connection)
+		reverseConnection := NewConnection(station2, station1, lineName)
+		s.connections = append(s.connections, reverseConnection)
+	} else {
+		panic(fmt.Sprintf("Invalid connection: [%s, %s, %s]", station1Name, station2Name, lineName))
+	}
+}
+
+func (s *Subway) HasConnection(station1Name string, station2Name string, lineName string) bool {
+	station1 := NewStation(station1Name)
+	station2 := NewStation(station2Name)
+	for _, connection := range s.connections {
+		if strings.EqualFold(connection.GetLineName(), lineName) {
+			if connection.GetStation1().Equals(station1) && connection.GetStation2().Equals(station2) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+/*
 func main() {
 	subway := NewSubway()
 
@@ -42,4 +72,14 @@ func main() {
 
 	subway.AddStation("ajax急流")
 	fmt.Println(len(subway.stations))
+
+	loader := NewSubwayLoader()
+	subway, err := loader.LoadFromFile("ObjectvilleSubway.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(len(subway.stations))
+	fmt.Println(len(subway.connections))
 }
+*/
